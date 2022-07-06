53a86fc552df14a3e456befc5a0d9ec22015a545
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2022-07-06 12:24:42 +0000

    added bet closing (late commit)

--- src/app/controller/betController.js
+++ src/app/controller/betController.js
@@ -9,6 +9,37 @@ import { getLog } from '../util/log';
 
 const log = getLog('betController');
 
+export const close = async (req, res) => {
+	const { projectId, actualResult } = req.body;
+	log('close', { projectId, actualResult });
+	const query = `UPDATE bet${'\n'
+		}SET actual_result = $2,${'\n'
+		}prize_total = prize_total + (CASE WHEN expected_result = $2 THEN ROUND(odd * bet, 2) ELSE 0 END)${'\n'
+		}WHERE project = $1${'\n'
+		}AND actual_result IS NULL${'\n'
+		}RETURNING *;`;
+	try {
+		const result = await dbQuery.query(query, [projectId, actualResult]);
+		const rows = result.rows;
+		log('close', { result });
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		log('close', { error });
+		if (error) {
+			if (error.message) {
+				errorMessage.error = error.message;
+			} else if (error.where) {
+				errorMessage.error = error.where;
+			} else {
+				errorMessage.error = error;
+			}
+		} else {
+			errorMessage.error = 'Unknown error.';
+		}
+		return res.status(status.error).send(errorMessage);
+	}
+};
+
 export const getList = async (req, res) => {
 	const { projectId } = req.query;
 	const query = 'SELECT project, date_time, home, away, odd, bet, expected_result, actual_result, bet_sum, bet_total, prize_total FROM bet WHERE project = $1 ORDER BY date_time DESC';
@@ -37,8 +68,16 @@ export const place = async (req, res) => {
 		}, $7 + (CASE WHEN expected_result = actual_result THEN 0 ELSE bet_sum END)${'\n' // bet_sum
 		}, bet_total + $7${'\n' // bet_total
 		}, prize_total${'\n' // prize_total
-		}FROM bet${'\n'
-		}WHERE project = $1${'\n'
+		}FROM (${'\n'
+		}	(${'\n'
+		}		SELECT project, date_time, home, away, expected_result, odd, bet, actual_result, bet_sum, bet_total, prize_total${'\n'
+		}		FROM bet${'\n'
+		}		WHERE project = $1${'\n'
+		}		ORDER BY date_time DESC${'\n'
+		}		LIMIT 1${'\n'
+		}	) ${'\n'
+		}	UNION (SELECT $1, '0001-01-01T00:00:00Z', NULL, NULL, $5, NULL, NULL, $5, 0, 0, 0)${'\n' // for when the table is empty
+		}) default_row${'\n'
 		}ORDER BY date_time DESC${'\n'
 		}LIMIT 1${'\n'
 		}RETURNING *;`;
--- src/app/route/betRoute.js
+++ src/app/route/betRoute.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 import verifyAuth from '../middleware/verifyAuth';
 
-import { getList, place } from '../controller/betController';
+import { close, getList, place } from '../controller/betController';
 
 const router = express.Router();
 
+router.post('/close', verifyAuth, close);
 router.get('/list', verifyAuth, getList);
 router.post('/place', verifyAuth, place);
 
